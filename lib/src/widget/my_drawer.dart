0706974d50bb4b0b9e0e643f9ebadf83974aeede
emmanes <Emma.nesvik@hotmail.com>
2025-02-02 16:17:17 +0100

    Wrapped Drawer to reduce width
    We still need to find an optimal width, but I have reduced it by a decent amount so that it's not as wide. The default size takes up almost the entire phone.

--- lib/src/widget/my_drawer.dart
+++ lib/src/widget/my_drawer.dart
@@ -9,78 +9,81 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Container(
-        color: Theme.of(context).colorScheme.surface,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: InkWell(
+    return SizedBox(
+      width: 240,
+      child: Drawer(
+        child: Container(
+          color: Theme.of(context).colorScheme.surface,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    AppRouter.isDrawerNavigation = true; // Mark navigation as from the drawer
+                    context.goNamed('loginPage');
+                    AppRouter.isDrawerNavigation = false; // Reset after navigation
+                  },
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.account_circle,
+                        color: Theme.of(context).colorScheme.tertiary,
+                        size: 36,
+                      ),
+                      const SizedBox(width: 12),
+                      Text(
+                        'Log in or sign up',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.tertiary,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      
+              const SizedBox(height: 20),
+      
+              ListTile(
+                leading: Icon(Icons.filter_list, color: Theme.of(context).colorScheme.tertiary),
+                title: Text('Filter', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
+                onTap: () {},
+              ),
+      
+              ListTile(
+                leading: Icon(Icons.star, color: Theme.of(context).colorScheme.tertiary),
+                title: Text('Wishlist', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
+                onTap: () {},
+              ),
+      
+              ListTile(
+                leading: Icon(Icons.chat, color: Theme.of(context).colorScheme.tertiary),
+                title: Text('Chat', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
                 onTap: () {
                   Navigator.of(context).pop();
                   AppRouter.isDrawerNavigation = true; // Mark navigation as from the drawer
-                  context.goNamed('loginPage');
-                  AppRouter.isDrawerNavigation = false; // Reset after navigation
+                  context.goNamed('chat');
+                  AppRouter.isDrawerNavigation = false;
                 },
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.account_circle,
-                      color: Theme.of(context).colorScheme.tertiary,
-                      size: 36,
-                    ),
-                    const SizedBox(width: 12),
-                    Text(
-                      'Log in or sign up',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.tertiary,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
               ),
-            ),
-
-            const SizedBox(height: 20),
-
-            ListTile(
-              leading: Icon(Icons.filter_list, color: Theme.of(context).colorScheme.tertiary),
-              title: Text('Filter', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
-              onTap: () {},
-            ),
-
-            ListTile(
-              leading: Icon(Icons.star, color: Theme.of(context).colorScheme.tertiary),
-              title: Text('Wishlist', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
-              onTap: () {},
-            ),
-
-            ListTile(
-              leading: Icon(Icons.chat, color: Theme.of(context).colorScheme.tertiary),
-              title: Text('Chat', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
-              onTap: () {
-                Navigator.of(context).pop();
-                AppRouter.isDrawerNavigation = true; // Mark navigation as from the drawer
-                context.goNamed('chat');
-                AppRouter.isDrawerNavigation = false;
-              },
-            ),
-
-            ListTile(
-              leading: Icon(Icons.calendar_today, color: Theme.of(context).colorScheme.tertiary),
-              title: Text('Meal planner', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
-              onTap: () {
-                Navigator.of(context).pop();
-                AppRouter.isDrawerNavigation = true; // Mark navigation as from the drawer
-                context.goNamed('mealPlanner');
-                AppRouter.isDrawerNavigation = false;
-              },
-            ),
-          ],
+      
+              ListTile(
+                leading: Icon(Icons.calendar_today, color: Theme.of(context).colorScheme.tertiary),
+                title: Text('Meal planner', style: TextStyle(color: Theme.of(context).colorScheme.tertiary)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  AppRouter.isDrawerNavigation = true; // Mark navigation as from the drawer
+                  context.goNamed('mealPlanner');
+                  AppRouter.isDrawerNavigation = false;
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
