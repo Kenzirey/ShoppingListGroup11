21133de585d043e4d461088866a1c6d36bda61eb
stenOskar <stenhalse@gmail.com>
2025-02-27 19:52:14 +0100

    Enhance ReceiptItem with quantity
    Also implemented total price computation

--- lib/models/receipt_item.dart
+++ lib/models/receipt_item.dart
@@ -1,15 +1,20 @@
 /// ReceiptItem model class
 class ReceiptItem {
   String name;
+  int quantity;
   double price;
   String? allergy;
 
   ReceiptItem({
     required this.name,
+    this.quantity = 1,
     required this.price,
     this.allergy,
   });
 
+  double get totalPrice => quantity * price;
+
   @override
-  String toString() => 'ReceiptItem(name: $name, price: $price, allergy: $allergy)';
+  String toString() =>
+      'ReceiptItem(name: $name, quantity: $quantity, unitPrice: $price, allergy: $allergy)';
 }
