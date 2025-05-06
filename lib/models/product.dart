435415cb6d02d0a2931f3c3cfa394479a38ef5c3
emmanes <Emma.nesvik@hotmail.com>
2025-05-06 21:23:07 +0200

    Removed unused code
    As this is deprecated, no longer used. Just removed it.

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,9 +1,6 @@
-import 'package:shopping_list_g11/data/measurement_type.dart';
-
 /// Represents one grocery item product.
 class Product {
   final String name;
-  final MeasurementType? measurementType;
   final DateTime purchaseDate;
   final String? price;
   final String amount;
@@ -11,7 +8,6 @@ class Product {
 
   const Product({
     required this.name,
-    this.measurementType,
     required this.purchaseDate,
     this.price,
     required this.amount,
@@ -25,12 +21,10 @@ class Product {
     required DateTime purchaseDate,
     String? price,
     required String amount,
-    MeasurementType? measurementType,
     String? category,
   }) {
     return Product(
       name: name,
-      measurementType: measurementType,
       purchaseDate: purchaseDate,
       price: price,
       amount: amount,
@@ -38,11 +32,3 @@ class Product {
     );
   }
 }
-
-
-
-
-// In regard to dietary things, it should probably be a tag on the product itself in database, so when OCR finds lactose free milk,
-// it adds a tag on it to indicate it is lactose free. But the name just remains "milk" not "lactose free milk"
-
-// changing from specific brand names to just milk etc is called normalize, and adding attributes is probably better longterm?
